e99610caa7bd7bcf20e3629e0796cb5d6dd21b4a
hurshik7 <hurshik7@gmail.com>
2023-01-08 03:54:28 -0800

    formatting

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>	// printf
-#include <unistd.h>	// fork, chdir
+#include <stdlib.h> // exit
 #include <string.h> // perror, strlen
 #include <sys/wait.h> // waitpid
-#include <stdlib.h> // exit
+#include <unistd.h>	// fork, chdir
 
-#define MAX 255
+#define MAX (255)
 
-void *prompt(char cBuf[]){
+void *prompt(char cBuf[])
+{
     void *ret;
 
-    printf("myshell > ");
+    printf("sh_shell > ");
     ret = fgets(cBuf, MAX, stdin);
 
-    if(cBuf[strlen(cBuf)-1] == '\n')
-        cBuf[strlen(cBuf)-1] = 0;
+    if (cBuf[strlen(cBuf)-1] == '\n') {
+        cBuf[strlen(cBuf) - 1] = 0;
+    }
     return ret;
 }
 
@@ -26,23 +28,29 @@ int main()
     int status;
     int fd[2];
 
-    while(prompt(cBuf)){	// while successful input
-        if((pid=fork())<0){
+    while (prompt(cBuf)) {	// while successful input
+        if ((pid=fork()) < 0) {
             perror("fork error");
         }
-        else if(pid ==0){	// child
+        else if (pid ==0) {	// child
             // 명령어 공백기준 분할
             arg[0] = strtok(cBuf," ");
             // 자른 문자 다음부터 구분자 또 찾기
             arg[1] = strtok(NULL, " ");
 
-            if(arg[1] == NULL){					//	(1) no argument
-                if(execvp(arg[0], arg) < 0){
+            if (strcmp(arg[0], "exit") == 0) {
+                printf("exit sh_shell\n");
+                exit(0);
+                // 이거 잘 안됨. 일단 공부부터!
+            }
+
+            if (arg[1] == NULL) {					//	(1) no argument
+                if (execvp(arg[0], arg) < 0) {
                     printf("%s: command not found\n",arg[0]);
                     exit(0);
                 }
             }
-            else if(*arg[1] != '|'){			// (2) one argument
+            else if (*arg[1] != '|') {			// (2) one argument
                 arg[2] = 0;
                 // cd command
                 if(strcmp(arg[0], "cd")==0) {
@@ -56,30 +64,31 @@ int main()
                 }
 
             }
-            else{								// (3) pipe
+            else {								// (3) pipe
                 arg[2] = strtok(NULL, " ");
                 arg[3] = 0;
 
                 pipe(fd);
 
-                if((pipe_p1=fork())==0){
+                if ((pipe_p1=fork()) == 0) {
                     close(1);		// close stdout
                     close(fd[0]);	// close pipe read
                     dup(fd[1]);		// dup pipe write at stdout
                     close(fd[1]);
 
-                    arg_p[0] = arg[0]; arg_p[1] = 0;
+                    arg_p[0] = arg[0];
+                    arg_p[1] = 0;
 
                     if(execvp(arg[0],arg_p) < 0){
                         printf("%s: command not found\n", arg[0]);
                         exit(0);
                     }
                 }
-                else if(pipe_p1<0){
+                else if (pipe_p1 < 0){
                     perror("fork error");
                 }
 
-                if((pipe_p2=fork())==0){
+                if((pipe_p2=fork()) == 0){
                     close(0);		// close stdin
                     close(fd[1]);	// close piepe write
                     dup(fd[0]);		// dup pipe read at stdin
@@ -95,7 +104,7 @@ int main()
                     perror("child2 execvp failed");
                     exit(1);
                 }
-                else if(pipe_p2<0){
+                else if(pipe_p2 < 0){
                     perror("fork error");
                 }
                 close(fd[1]);
