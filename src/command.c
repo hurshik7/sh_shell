a9c905b5ffe74702eaeee51f718a12f94217205c
hurshik7 <hurshik7@gmail.com>
2023-01-19 22:18:37 -0800

    add TODOs

--- src/command.c
+++ src/command.c
@@ -5,12 +5,16 @@
 
 int parse_command_malloc(const char *line, struct command *cmd)
 {
-    // TODO redirection parsing -> darcy design 그냥 따르자. 잘 모르는거 같은데 더 제대로 하려면 복잡해짐
-    // TODO wordexp() 해주기 (이때는 redirection < > 이 없는 상태, redirection 지시문 전이 \0로 바껴있음)
 
     size_t count = 0;
     cmd->argv = tokenize_malloc(line, " ", &count);
     cmd->argc = count;
+
+    // TODO redirection parsing -> 완료 -> iterm꺼에서 가져다 쓰기
+    // TODO wordexp(): command->argv 돌면서 다시 malloc, free하면서 wordexp() 하는데 여러개 나오면 추가하고, < Or > 가 들어가거나 or redirect_file들과 strcmp값이 0이면 제
+    // TODO command_t에 char** 형 하나 추가해야 할듯! 그러면 여기서 할일은 끝!
+
+    // TODO redirect() 함수 적절한 파일에 만들고 dup2() 할 때 redirection_filename을 realpath()돌려서 Open하기!
 
     return 0;
 }
